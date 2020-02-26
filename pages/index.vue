1400dd342b11c5c7acd4e033c22e657ffd1b3eac
Louis Sandoval <louis@sandoval.com>
2020-02-26 16:19:02 -0700

    Add some intro text

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <template>
 	<section>
-		<div></div>
+		<div class="columns">
+			<div class="column">
+				<div class="is-size-4">Welcome!</div>
+				<p>
+					The Maricopa Amateur Radio Association serves the Amateur
+					Radio community in the City of Maricopa and the communities
+					of Casa Grande, Hidden Valley, Stanfield, Arizona City, and
+					Eloy, as well as surrounding areas. We provide VE testing
+					and a variety of community services through our affiliation
+					with the Community Emergency Response Team (CERT) and
+					Military Affiliate Radio Service (MARS).
+					<br /><br />
+					We are a registered nonprofit corporation. We sustain out
+					activities with donations, fundraising, and membership dues.
+					Donations to the club are always appreciated and are tax
+					deductible.
+				</p>
+			</div>
+		</div>
 	</section>
 </template>
 
@@ -10,6 +28,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style></style>

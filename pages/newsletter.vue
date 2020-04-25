2b5bef178a6757f75f9b942c05d0bf3eff3a97d0
Louis Sandoval <louis@sandoval.com>
2020-04-25 12:11:16 -0700

    Create Newsletter page

---
+++ pages/newsletter.vue
@@ -0,0 +1,40 @@
+<template>
+	<section class="has-text-centered">
+		<div class="columns">
+			<div class="column">
+				<h1 class="is-size-1">
+					Join our mailing list!
+				</h1>
+				<div>
+					{{ settings.main_text }}
+				</div>
+				<div>
+					<a
+						href="https://copahams.us4.list-manage.com/subscribe?u=5ab31254779c7186ff0c9b8c8&id=fa560aca8b"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<button class="button">
+							Join
+						</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+import newsletterSettings from "../assets/settings/pages/newsletter.json";
+
+export default {
+	name: "newsletter",
+	data() {
+		return {
+			settings: newsletterSettings
+		};
+	}
+};
+</script>
+
+<style scoped></style>

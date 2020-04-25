5f7811e3d8ad12c59c9b8110d1bc0cd65606a1e5
Louis Sandoval <louis@sandoval.com>
2020-04-25 16:00:48 -0700

    Create By-Laws page

---
+++ pages/by_laws.vue
@@ -0,0 +1,27 @@
+<template>
+	<section class="has-text-centered">
+		<h1 class="is-size-1">By-Laws</h1>
+		<a
+			href="/documents/by_laws.pdf"
+			target="_blank"
+			rel="noreferrer noopener"
+			>Downloadable PDF Version</a
+		>
+		<iframe
+			width="800"
+			height="400"
+			frameborder="0"
+      title="By-Laws Embedded View"
+			src="https://docs.google.com/document/d/1ITV-1JpHi8ZiIp4eemKVYptAI4MVd88ivUzZb3fg2Ss/pub?widget=true&embedded=true"
+		>
+		</iframe>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "by_laws"
+};
+</script>
+
+<style scoped></style>

a769a6a088d4f7e2220d888e49611b651c129757
Louis Sandoval <louis@sandoval.com>
2020-04-25 16:21:33 -0700

    Create Constitution page

---
+++ pages/constitution.vue
@@ -0,0 +1,38 @@
+<template>
+	<section class="has-text-centered">
+		<div class="columns">
+			<div class="column">
+				<h1 class="is-size-1">Constitution</h1>
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<a
+					href="/documents/constitution.pdf"
+					target="_blank"
+					rel="noreferrer noopener"
+					>Downloadable PDF Version</a
+				>
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<iframe
+					width="800"
+					height="800"
+					src="https://docs.google.com/document/d/1EUlAt0Q8UfXkZhxP-W2KUlM23sZBkdIbQJNFjmwdYSU/pub?embedded=true"
+					frameborder="0"
+				>
+				</iframe>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "constitution"
+};
+</script>
+
+<style scoped></style>

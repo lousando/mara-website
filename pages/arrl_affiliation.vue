13a62bc6d77dfd4f439cef7c2cc3865fb1c0196d
Louis Sandoval <louis@sandoval.com>
2020-04-25 16:29:32 -0700

    Create ARRL Affiliation page

---
+++ pages/arrl_affiliation.vue
@@ -0,0 +1,30 @@
+<template>
+	<section class="has-text-centered">
+		<div class="columns">
+			<div class="column">
+				<h1 class="is-size-1">ARRL Club Charter</h1>
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<img
+					class="club-charter-certificate"
+					src="/imgs/arrl_charter.jpg"
+					alt="ARRL Club Charter Certificate"
+				/>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "arrl_affiliation"
+};
+</script>
+
+<style scoped>
+.club-charter-certificate {
+	max-width: 800px;
+}
+</style>

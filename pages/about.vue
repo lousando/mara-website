4fe54c3b25ba6cb731244f6c15528777a56c7228
Louis Sandoval <louis@sandoval.com>
2020-04-26 13:53:32 -0700

    Create About page

--- pages/about.vue
+++ pages/about.vue
@@ -12,10 +12,10 @@
 				>
 					<div class="card-content">
 						<div class="media">
-							<div class="media-left">
-								<figure class="image is-48x48">
+							<div class="media-left" v-if="officer.image_src">
+								<figure class="image is-128x128">
 									<img
-										src="https://bulma.io/images/placeholders/96x96.png"
+										:src="officer.image_src"
 										:alt="'photo of ' + officer.name"
 									/>
 								</figure>
@@ -29,43 +29,47 @@
 								</p>
 							</div>
 						</div>
-						<div class="content">
+						<p class="content">
 							{{ officer.bio }}
-						</div>
+						</p>
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="columns">
 			<div class="column">
 				<h1 class="is-size-1">
 					Board Members
 				</h1>
-				<div
-					class="card"
-					v-for="member in settings.board_members"
-					:key="member.call_sign"
-				>
-					<div class="card-content">
-						<div class="media">
-							<div class="media-left">
-								<figure class="image is-48x48">
-									<img
-										src="https://bulma.io/images/placeholders/96x96.png"
-										:alt="'photo of ' + member.name"
-									/>
-								</figure>
+				<div class="is-flex-desktop">
+					<div
+						class="card"
+						v-for="member in settings.board_members"
+						:key="member.call_sign"
+					>
+						<div class="card-content">
+							<div class="media">
+								<div class="media-left" v-if="member.image_src">
+									<figure class="image is-48x48">
+										<img
+											src="https://bulma.io/images/placeholders/96x96.png"
+											:alt="'photo of ' + member.name"
+										/>
+									</figure>
+								</div>
+								<div class="media-content">
+									<p class="title is-4">
+										{{ member.name }},
+										{{ member.call_sign }}
+									</p>
+									<p class="subtitle is-6">
+										Board member
+									</p>
+								</div>
 							</div>
-							<div class="media-content">
-								<p class="title is-4">
-									{{ member.name }},
-									{{ member.call_sign }}
-								</p>
-								<p class="subtitle is-6">
-									Board member
-								</p>
+							<div class="content">
+								{{ member.bio }}
 							</div>
-						</div>
-						<div class="content">
-							{{ member.bio }}
 						</div>
 					</div>
 				</div>

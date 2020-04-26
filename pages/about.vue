bb35b0f36641c866c0d7573d75a4a1af4449db30
Louis Sandoval <louis@sandoval.com>
2020-04-26 14:52:20 -0700

    Add VE Team members to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,9 @@
 <template>
 	<section>
+		<!-- Officers -->
 		<div class="columns">
 			<div class="column">
-				<h1 class="is-size-1">
+				<h1 id="officers" class="is-size-1">
 					Officers
 				</h1>
 				<div
@@ -21,12 +22,12 @@
 								</figure>
 							</div>
 							<div class="media-content">
-								<p class="title is-4">
+								<div class="title is-4">
 									{{ officer.name }}, {{ officer.call_sign }}
-								</p>
-								<p class="subtitle is-6">
+								</div>
+								<div class="subtitle is-6">
 									{{ officer.role }}
-								</p>
+								</div>
 							</div>
 						</div>
 						<p class="content">
@@ -36,12 +37,13 @@
 				</div>
 			</div>
 		</div>
+		<!-- Board Members -->
 		<div class="columns">
 			<div class="column">
-				<h1 class="is-size-1">
+				<h1 id="board-members" class="is-size-1">
 					Board Members
 				</h1>
-				<div class="is-flex-desktop">
+				<div class="board-members-container">
 					<div
 						class="card"
 						v-for="member in settings.board_members"
@@ -50,25 +52,62 @@
 						<div class="card-content">
 							<div class="media">
 								<div class="media-left" v-if="member.image_src">
-									<figure class="image is-48x48">
+									<figure class="image is-128x128">
 										<img
-											src="https://bulma.io/images/placeholders/96x96.png"
+											:src="member.image_src"
 											:alt="'photo of ' + member.name"
 										/>
 									</figure>
 								</div>
 								<div class="media-content">
-									<p class="title is-4">
+									<div class="title is-4">
 										{{ member.name }},
 										{{ member.call_sign }}
-									</p>
-									<p class="subtitle is-6">
+									</div>
+									<div class="subtitle is-6">
 										Board member
-									</p>
+									</div>
 								</div>
 							</div>
 							<div class="content">
 								{{ member.bio }}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- VE Team -->
+		<div class="columns">
+			<div class="column">
+				<h1 id="ve-team" class="is-size-1">
+					Volunteer Examiner Team
+				</h1>
+				<div class="ve-container">
+					<div
+						class="card"
+						v-for="ve in settings.ve_team"
+						:key="ve.call_sign"
+					>
+						<div class="card-content">
+							<div class="media">
+								<div class="media-left" v-if="ve.image_src">
+									<figure class="image is-128x128">
+										<img
+											:src="ve.image_src"
+											:alt="'photo of ' + ve.name"
+										/>
+									</figure>
+								</div>
+								<div class="media-content">
+									<div class="title is-4">
+										{{ ve.name }},
+										{{ ve.call_sign }}
+									</div>
+									<div class="subtitle is-6">
+										Volunteer Examiner
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -80,15 +119,45 @@
 
 <script>
 import aboutSettings from "../assets/settings/pages/about.json";
+import _orderBy from "lodash/orderBy";
+import gravatarUrl from "gravatar-url";
+
+const orderedBoardMembers = _orderBy(aboutSettings.board_members, "name");
+const orderedVeTeam = _orderBy(aboutSettings.ve_team, "name");
 
 export default {
 	name: "about",
 	data() {
 		return {
-			settings: aboutSettings
+			settings: {
+				officers: aboutSettings.officers.map(_addImgSrc),
+				board_members: orderedBoardMembers.map(_addImgSrc),
+				ve_team: orderedVeTeam.map(_addImgSrc)
+			}
 		};
 	}
 };
+
+function _addImgSrc(member) {
+	if (member.hasOwnProperty("image_src")) {
+		return member;
+	}
+
+	return {
+		...member,
+		image_src: gravatarUrl(member.call_sign, {
+			default: "identicon",
+			size: "256"
+		})
+	};
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.board-members-container,
+.ve-container {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+}
+</style>

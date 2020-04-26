47c7f068ab07675957b82053e6a5e2bd5117fb28
Louis Sandoval <louis@sandoval.com>
2020-04-25 20:44:07 -0700

    Create stub for Membership page

---
+++ pages/membership.vue
@@ -0,0 +1,18 @@
+<template>
+	<section></section>
+</template>
+
+<script>
+import membershipSettings from "../assets/settings/pages/meetings.json";
+
+export default {
+	name: "membership",
+	data() {
+		return {
+			settings: membershipSettings
+		};
+	}
+};
+</script>
+
+<style scoped></style>

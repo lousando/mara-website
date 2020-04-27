7112458a3963de15a415ca9cd626149085002794
Louis Sandoval <louis@sandoval.com>
2020-04-26 18:23:32 -0700

    Add box around membership page elements

--- pages/membership.vue
+++ pages/membership.vue
@@ -1,61 +1,63 @@
 <template>
 	<section class="has-text-centered">
-		<div class="columns">
-			<div class="column">
-				<h1 class="is-size-1">
-					Annual Membership
-				</h1>
+		<div class="box">
+			<div class="columns">
+				<div class="column">
+					<h1 class="is-size-1">
+						Annual Membership
+					</h1>
+				</div>
 			</div>
-		</div>
-    <div class="columns">
-      <div class="column">
-        <p>
-          {{settings.main_text}}
-        </p>
-      </div>
-    </div>
-		<div class="columns">
-			<div class="column">
-				<form
-					action="https://www.paypal.com/cgi-bin/webscr"
-					method="post"
-					target="_blank"
-				>
-					<!-- hidden fields -->
-					<input type="hidden" name="cmd" value="_s-xclick" />
-					<input
-						type="hidden"
-						name="hosted_button_id"
-						value="SWJ6RDWGJY7X6"
-					/>
-					<input type="hidden" name="on0" value="ANNUAL dues" />
-					<input type="hidden" name="currency_code" value="USD" />
+			<div class="columns">
+				<div class="column">
+					<p>
+						{{ settings.main_text }}
+					</p>
+				</div>
+			</div>
+			<div class="columns">
+				<div class="column">
+					<form
+						action="https://www.paypal.com/cgi-bin/webscr"
+						method="post"
+						target="_blank"
+					>
+						<!-- hidden fields -->
+						<input type="hidden" name="cmd" value="_s-xclick" />
+						<input
+							type="hidden"
+							name="hosted_button_id"
+							value="SWJ6RDWGJY7X6"
+						/>
+						<input type="hidden" name="on0" value="ANNUAL dues" />
+						<input type="hidden" name="currency_code" value="USD" />
 
-					<div class="columns">
-						<div class="column">
-							<div class="select">
-								<select name="os0">
-									<option value="Single member">
-										Single member: $25.00 USD/year
-									</option>
-									<option value="Family membership">
-										Family membership: $30.00 USD/year
-									</option>
-								</select>
+						<div class="columns">
+							<div class="column">
+								<div class="select">
+									<select name="os0">
+										<option value="Single member">
+											Single member: $25.00 USD/year
+										</option>
+										<option value="Family membership">
+											Family membership: $30.00 USD/year
+										</option>
+									</select>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="columns">
-						<div class="column">
-							<button
-								type="submit"
-								class="square-button square-button--yellow"
-							>
-								Join
-							</button>
+						<div class="columns">
+							<div class="column">
+								<button
+									type="submit"
+									class="square-button square-button--yellow"
+								>
+									Join
+								</button>
+							</div>
 						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			</div>
 		</div>
 	</section>

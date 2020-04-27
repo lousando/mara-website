5ebb37b735523b0833e3cfb1f0816e1a4a604f89
Louis Sandoval <louis@sandoval.com>
2020-04-26 18:26:58 -0700

    Add box around constitution and by-laws page elements

--- pages/constitution_and_by_laws.vue
+++ pages/constitution_and_by_laws.vue
@@ -3,37 +3,41 @@
 		<div class="columns">
 			<!-- Constitution -->
 			<div class="column">
-				<h1 class="is-size-1">Constitution</h1>
-				<div>
-					<a
-						href="/documents/constitution.pdf"
-						target="_blank"
-						rel="noreferrer noopener"
-						>Downloadable PDF Version</a
+				<div class="box">
+					<h1 class="is-size-1">Constitution</h1>
+					<div>
+						<a
+							href="/documents/constitution.pdf"
+							target="_blank"
+							rel="noreferrer noopener"
+							>Downloadable PDF Version</a
+						>
+					</div>
+					<iframe
+						title="Constitution Embedded View"
+						src="https://docs.google.com/document/d/1EUlAt0Q8UfXkZhxP-W2KUlM23sZBkdIbQJNFjmwdYSU/pub?embedded=true"
 					>
+					</iframe>
 				</div>
-				<iframe
-					title="Constitution Embedded View"
-					src="https://docs.google.com/document/d/1EUlAt0Q8UfXkZhxP-W2KUlM23sZBkdIbQJNFjmwdYSU/pub?embedded=true"
-				>
-				</iframe>
 			</div>
 			<!-- By-Laws -->
 			<div class="column">
-				<h1 class="is-size-1">By-Laws</h1>
-				<div>
-					<a
-						href="/documents/by_laws.pdf"
-						target="_blank"
-						rel="noreferrer noopener"
-						>Downloadable PDF Version</a
+				<div class="box">
+					<h1 class="is-size-1">By-Laws</h1>
+					<div>
+						<a
+							href="/documents/by_laws.pdf"
+							target="_blank"
+							rel="noreferrer noopener"
+							>Downloadable PDF Version</a
+						>
+					</div>
+					<iframe
+						title="By-Laws Embedded View"
+						src="https://docs.google.com/document/d/1ITV-1JpHi8ZiIp4eemKVYptAI4MVd88ivUzZb3fg2Ss/pub?widget=true&embedded=true"
 					>
+					</iframe>
 				</div>
-				<iframe
-					title="By-Laws Embedded View"
-					src="https://docs.google.com/document/d/1ITV-1JpHi8ZiIp4eemKVYptAI4MVd88ivUzZb3fg2Ss/pub?widget=true&embedded=true"
-				>
-				</iframe>
 			</div>
 		</div>
 	</section>

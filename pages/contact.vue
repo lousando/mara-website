375ccaf40e1d2eb7397d3ef72e98ac001dbef3e3
Louis Sandoval <louis@sandoval.com>
2020-04-26 19:13:38 -0700

    Add "reasons" option to contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,7 +7,7 @@
 						Contact Us
 					</h1>
 					<h2 class="is-size-2">
-						Questions? Comments? Feel free to reach out.
+						Feel free to reach out.
 					</h2>
 				</div>
 			</div>
@@ -56,6 +56,30 @@
 							</div>
 						</div>
 						<div class="field">
+							<label class="label">Reason</label>
+							<div class="select">
+								<select v-model="reason" required="required">
+									<option
+										selected="selected"
+										value=""
+										disabled="disabled"
+										>Please select an option</option
+									>
+									<option value="info"
+										>Question, Comment, or Inquiry</option
+									>
+									<option value="mvet">VE Testing</option>
+									<option value="webmaster"
+										>Website Issues</option
+									>
+									<option value="info">Other</option>
+								</select>
+							</div>
+							<div class="help is-danger">
+								This field is required
+							</div>
+						</div>
+						<div class="field">
 							<label class="label">Message</label>
 							<div class="control">
 								<textarea
@@ -70,21 +94,13 @@
 							</div>
 						</div>
 						<div class="has-text-centered">
-							<button class="button" type="submit">
+							<button class="button is-primary" type="submit">
 								Send
 							</button>
 						</div>
 					</form>
 				</div>
 				<div class="column">
-					<div>
-						<h3 class="is-size-3">
-							Email:
-						</h3>
-						<a :href="'mailto:' + mara_inquiry_email">{{
-							mara_inquiry_email
-						}}</a>
-					</div>
 					<div>
 						<h3 class="is-size-3">
 							Mailing Address:
@@ -95,12 +111,6 @@
 							Maricopa, AZ<br />
 							85139<br />
 						</div>
-					</div>
-					<div>
-						<h3 class="is-size-3">
-							Phone:
-						</h3>
-						<a href="tel:1-520-222-7646">​(520) 222-7646</a>
 					</div>
 				</div>
 			</div>
@@ -118,15 +128,16 @@ export default {
 			firstName: "",
 			lastName: "",
 			phone: "",
+			reason: "",
 			message: ""
 		};
 	},
 	methods: {
 		onClickSubmit: function() {
 			const mailToLink = `mailto:${
-				this.$store.state.globalSettings.mara_inquiry_email
-			}?subject=${encodeURIComponent(
-				"CopaHams Info Request"
+				this.reason
+			}@copahams.org?subject=${encodeURIComponent(
+				`CopaHams ${this.reason.toUpperCase()} Request`
 			)}&body=${encodeURIComponent(this.message)}<br/><br/>Phone: ${
 				this.phone
 			}<br/>- ${this.firstName} ${this.lastName}`;
@@ -142,5 +153,9 @@ form {
 	margin: auto;
 	width: 500px;
 	max-width: 80%;
+}
+
+textarea {
+	height: 150px;
 }
 </style>

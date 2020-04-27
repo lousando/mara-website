43272261d161b9f66c846b36c7c5d3620e4e6c83
Louis Sandoval <louis@sandoval.com>
2020-04-26 18:22:05 -0700

    Add box around newsletter page elements

--- pages/newsletter.vue
+++ pages/newsletter.vue
@@ -2,22 +2,24 @@
 	<section class="has-text-centered">
 		<div class="columns">
 			<div class="column">
-				<h1 class="is-size-1">
-					Join our mailing list!
-				</h1>
-				<div>
-					{{ settings.main_text }}
-				</div>
-				<div>
-					<a
-						href="https://copahams.us4.list-manage.com/subscribe?u=5ab31254779c7186ff0c9b8c8&id=fa560aca8b"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<button class="button">
-							Join
-						</button>
-					</a>
+				<div class="box">
+					<h1 class="is-size-1">
+						Join our mailing list!
+					</h1>
+					<div>
+						{{ settings.main_text }}
+					</div>
+					<div>
+						<a
+							href="https://copahams.us4.list-manage.com/subscribe?u=5ab31254779c7186ff0c9b8c8&id=fa560aca8b"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<button class="button is-primary">
+								Join
+							</button>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>

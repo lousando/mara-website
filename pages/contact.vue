04a867b5bc96abbe4484c4d5684cd3fb18c765bc
Louis Sandoval <louis@sandoval.com>
2020-04-26 18:29:40 -0700

    Add box around contact page elements

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,105 +1,107 @@
 <template>
 	<section>
-		<div class="columns has-text-centered">
-			<div class="column">
-				<h1 class="is-size-1">
-					Contact Us
-				</h1>
-				<h2 class="is-size-2">
-					Questions? Comments? Feel free to reach out.
-				</h2>
-			</div>
-		</div>
-		<div class="columns">
-			<div class="column">
-				<form @submit.prevent="onClickSubmit">
-					<div class="field">
-						<label class="label">First Name</label>
-						<div class="control">
-							<input
-								class="input"
-								type="text"
-								v-model="firstName"
-								autocomplete="given-name"
-								required="required"
-							/>
-						</div>
-						<div class="help is-danger">
-							This field is required
-						</div>
-					</div>
-					<div class="field">
-						<label class="label">Last Name</label>
-						<div class="control">
-							<input
-								class="input"
-								type="text"
-								v-model="lastName"
-								autocomplete="family-name"
-								required="required"
-							/>
-						</div>
-						<div class="help is-danger">
-							This field is required
-						</div>
-					</div>
-					<div class="field">
-						<label class="label">Phone</label>
-						<div class="control">
-							<input
-								class="input"
-								type="tel"
-								v-model="phone"
-								autocomplete="tel"
-							/>
-						</div>
-					</div>
-					<div class="field">
-						<label class="label">Message</label>
-						<div class="control">
-							<textarea
-								class="input"
-								rows="5"
-								required="required"
-								v-model="message"
-							/>
-						</div>
-						<div class="help is-danger">
-							This field is required
-						</div>
-					</div>
-					<div class="has-text-centered">
-						<button class="button" type="submit">
-							Send
-						</button>
-					</div>
-				</form>
-			</div>
-			<div class="column">
-				<div>
-					<h3 class="is-size-3">
-						Email:
-					</h3>
-					<a :href="'mailto:' + mara_inquiry_email">{{
-						mara_inquiry_email
-					}}</a>
+		<div class="box">
+			<div class="columns has-text-centered">
+				<div class="column">
+					<h1 class="is-size-1">
+						Contact Us
+					</h1>
+					<h2 class="is-size-2">
+						Questions? Comments? Feel free to reach out.
+					</h2>
 				</div>
-				<div>
-					<h3 class="is-size-3">
-						Mailing Address:
-					</h3>
-					<div class="has-text-left">
-						Maricopa Amateur Radio Association<br />
-						Post Office Box 951<br />
-						Maricopa, AZ<br />
-						85139<br />
-					</div>
+			</div>
+			<div class="columns">
+				<div class="column">
+					<form @submit.prevent="onClickSubmit">
+						<div class="field">
+							<label class="label">First Name</label>
+							<div class="control">
+								<input
+									class="input"
+									type="text"
+									v-model="firstName"
+									autocomplete="given-name"
+									required="required"
+								/>
+							</div>
+							<div class="help is-danger">
+								This field is required
+							</div>
+						</div>
+						<div class="field">
+							<label class="label">Last Name</label>
+							<div class="control">
+								<input
+									class="input"
+									type="text"
+									v-model="lastName"
+									autocomplete="family-name"
+									required="required"
+								/>
+							</div>
+							<div class="help is-danger">
+								This field is required
+							</div>
+						</div>
+						<div class="field">
+							<label class="label">Phone</label>
+							<div class="control">
+								<input
+									class="input"
+									type="tel"
+									v-model="phone"
+									autocomplete="tel"
+								/>
+							</div>
+						</div>
+						<div class="field">
+							<label class="label">Message</label>
+							<div class="control">
+								<textarea
+									class="input"
+									rows="5"
+									required="required"
+									v-model="message"
+								/>
+							</div>
+							<div class="help is-danger">
+								This field is required
+							</div>
+						</div>
+						<div class="has-text-centered">
+							<button class="button" type="submit">
+								Send
+							</button>
+						</div>
+					</form>
 				</div>
-				<div>
-					<h3 class="is-size-3">
-						Phone:
-					</h3>
-					<a href="tel:1-520-222-7646">​(520) 222-7646</a>
+				<div class="column">
+					<div>
+						<h3 class="is-size-3">
+							Email:
+						</h3>
+						<a :href="'mailto:' + mara_inquiry_email">{{
+							mara_inquiry_email
+						}}</a>
+					</div>
+					<div>
+						<h3 class="is-size-3">
+							Mailing Address:
+						</h3>
+						<div class="has-text-left">
+							Maricopa Amateur Radio Association<br />
+							Post Office Box 951<br />
+							Maricopa, AZ<br />
+							85139<br />
+						</div>
+					</div>
+					<div>
+						<h3 class="is-size-3">
+							Phone:
+						</h3>
+						<a href="tel:1-520-222-7646">​(520) 222-7646</a>
+					</div>
 				</div>
 			</div>
 		</div>

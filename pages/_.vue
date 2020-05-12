cc09aa84b7097c80b7bf967d821e1b30737e7262
Louis Sandoval <louis@sandoval.com>
2020-05-11 19:20:28 -0700

    Add 404 page

---
+++ pages/_.vue
@@ -0,0 +1,32 @@
+<template>
+	<section class="has-text-centered">
+		<div class="columns">
+			<div class="column">
+				<h1
+					class="is-size-1 has-text-white has-background-black-transparent"
+				>
+					Page Not Found!
+				</h1>
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<div class="box">
+					<div class="content">
+						<p>
+							​{{ this.$store.state.globalSettings.error_404_message }}
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "error_404"
+};
+</script>
+
+<style scoped></style>

d3c1e7c01f1d12c6ef97e5a6f86849779b30f347
Louis Sandoval <louis@sandoval.family>
2020-12-05 21:56:35 -0700

    Redo the email form to use a back-end service to send messages

--- pages/contact.vue
+++ pages/contact.vue
@@ -22,7 +22,7 @@
 			</div>
 			<div class="columns">
 				<div class="column">
-					<form @submit.prevent="onClickSubmit">
+					<form @submit.prevent="onClickSubmit" v-if="!emailSent">
 						<div class="field">
 							<label class="label">First Name</label>
 							<div class="control">
@@ -54,6 +54,21 @@
 							</div>
 						</div>
 						<div class="field">
+							<label class="label">Email</label>
+							<div class="control">
+								<input
+									class="input"
+									type="email"
+									v-model="email"
+									autocomplete="email"
+									required="required"
+								/>
+							</div>
+							<div class="help is-danger">
+								This field is required
+							</div>
+						</div>
+						<div class="field">
 							<label class="label">Phone</label>
 							<div class="control">
 								<input
@@ -72,14 +87,15 @@
 										selected="selected"
 										value=""
 										disabled="disabled"
-										>Please select an option
+									>
+										Please select an option
 									</option>
-									<option value="info"
-										>Question, Comment, or Inquiry
+									<option value="info">
+										Question, Comment, or Inquiry
 									</option>
 									<option value="mvet">VE Testing</option>
-									<option value="webmaster"
-										>Website Issues
+									<option value="webmaster">
+										Website Issues
 									</option>
 									<option value="info">Other</option>
 								</select>
@@ -103,25 +119,32 @@
 							</div>
 						</div>
 						<div class="has-text-centered">
-							<button class="button is-primary" type="submit">
+							<button
+								:class="{
+									button: true,
+									'is-primary': true,
+									'is-loading': emailIsSending,
+								}"
+								type="submit"
+							>
 								Send
 							</button>
 						</div>
 					</form>
+					<div class="has-text-centered" v-else>
+						<h3 class="is-size-3">Email Successfully Sent</h3>
+						<p>We'll get back to you shortly.</p>
+					</div>
 				</div>
 				<div class="column">
 					<div>
-						<h3 class="is-size-3">
-							Phone:
-						</h3>
+						<h3 class="is-size-3">Phone:</h3>
 						<div class="has-text-left">
 							​<a href="tel:+​1-520-222-7646">​(520) 222-7646</a>
 						</div>
 					</div>
 					<div>
-						<h3 class="is-size-3">
-							Mailing Address:
-						</h3>
+						<h3 class="is-size-3">Mailing Address:</h3>
 						<div class="has-text-left">
 							Maricopa Amateur Radio Association<br />
 							Post Office Box 951<br />
@@ -137,6 +160,10 @@
 
 <script>
 import infoNotification from "../components/info-notification";
+import emailjs from "emailjs-com";
+
+// init service
+emailjs.init(process.env.NUXT_ENV_EMAILJS_USER_ID);
 
 export default {
 	head: {
@@ -144,11 +171,16 @@ export default {
 	},
 	data() {
 		return {
+			// email
 			firstName: "",
 			lastName: "",
+			email: "",
 			phone: "",
 			reason: "",
 			message: "",
+			// email status
+			emailIsSending: false,
+			emailSent: false,
 		};
 	},
 	components: {
@@ -156,15 +188,32 @@ export default {
 	},
 	methods: {
 		onClickSubmit: function () {
-			const mailToLink = `mailto:${
-				this.reason
-			}@copahams.org?subject=${encodeURIComponent(
-				`CopaHams ${this.reason.toUpperCase()} Request`
-			)}&body=${encodeURIComponent(this.message)}<br/><br/>Phone: ${
-				this.phone
-			}<br/>- ${this.firstName} ${this.lastName}`;
+			this.emailIsSending = true;
 
-			window.open(mailToLink);
+			emailjs
+				.send(
+					process.env.NUXT_ENV_EMAILJS_SERVICE_ID,
+					process.env.NUXT_ENV_EMAILJS_TEMPLATE_ID,
+					{
+						to_email: this.reason,
+						first_name: this.firstName,
+						last_name: this.lastName,
+						reply_to: this.email,
+						phone: this.phone,
+						message: this.message,
+					}
+				)
+				.then(() => {
+					this.emailSent = true;
+				})
+				.catch(() => {
+					alert(
+						"There was an issue sending your message. Please try again later."
+					);
+				})
+				.finally(() => {
+					this.emailIsSending = false;
+				});
 		},
 	},
 };

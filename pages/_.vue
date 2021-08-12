253f5c79d4426183142c0778d066d1e083b80243
Louis Sandoval <louis@sandoval.family>
2021-08-11 22:34:19 -0700

    Fix 404 page
    - It wasn't showing up

---
+++ pages/_.vue
@@ -0,0 +1,31 @@
+<template>
+	<section data-v-314f53c6="">
+		<div class="columns">
+			<div class="column has-text-centered">
+				<h1
+					class="
+						is-size-1
+						has-text-white has-background-black-transparent
+					"
+				>
+					Page Not Found!
+				</h1>
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<div class="box has-text-centered">
+					{{ this.$store.state.globalSettings.error_404_message }}
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	head: {
+		title: "404",
+	},
+};
+</script>

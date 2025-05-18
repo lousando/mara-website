687f3904d97a33df2b1f3bda5c2cfb5f60aa5688
Louis Sandoval <louis@sandoval.family>
2025-05-17 21:45:30 -0600

    Change Ham Radio Online link to HTTPS

--- pages/index.vue
+++ pages/index.vue
@@ -50,7 +50,7 @@
 					</info-notification>
           <img src="/imgs/ham_test_online_logo.png" alt="Ham Test Online Logo" />
           <p class="has-text-centered">
-            <a href="http://www.hamradiolicenseexam.com/">http://www.hamradiolicenseexam.com/</a>
+            <a href="https://www.hamradiolicenseexam.com/">https://www.hamradiolicenseexam.com/</a>
           </p>
 				</div>
 			</div>
